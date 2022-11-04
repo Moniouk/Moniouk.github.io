9d74a3d6a602bea1aef4a2865faf3219b0cfaf25
Moniouk <mouk0001@student.monash.edu>
2022-11-04 17:00:22 +1100

    test 4/11/22

--- cookbook/main.js
+++ cookbook/main.js
@@ -2,6 +2,7 @@ $(document).ready( function() {
 
     $(".splash").click(function() {
         $(".splash").addClass("remove");
+        $(".cracktrack")[0].play();
     });
     
     // Sound effects 
@@ -47,6 +48,97 @@ $(document).ready( function() {
         $(".cutbreadtrack")[0].play();
     });
 
+    $(".onioncut").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".oniontrack")[0].play();
+    });
+
+    $(".gingercut").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".gingertrack")[0].play();
+    });
+
+    $(".chillicut").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".chillitrack")[0].play();
+    });
+
+    $(".oilpour").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".oiltrack")[0].play();
+    });
+
+    $(".oilfry").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".oilfrytrack")[0].play();
+    });
+
+    
+    $(".mashsound").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".mashtrack")[0].play();
+    });
+
+    $(".mintsound").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".mashtrack")[0].play();
+    });
+
+    
+    $(".saltsound").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".salttrack")[0].play();
+    });
+
+    $(".fishcut").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".fishtrack")[0].play();
+    });
+
+    $(".beategg").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".beateggtrack")[0].play();
+    });
+
+    $(".mortarsound").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".mortartrack")[0].play();
+    });
+
+    $(".deepfry").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".deepfrytrack")[0].play();
+    });
+
+    $(".plop").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".ploptrack")[0].play();
+    });
+
+    $(".berrysquish").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".berrytrack")[0].play();
+    });
+
+    $(".clean").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".cleantrack")[0].play();
+    });
+
+    $(".boil").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".boiltrack")[0].play();
+    });
+
+    $(".intestinesound").mouseenter(function() {
+        console.log("mouse entering crack");
+        $(".intestinetrack")[0].play();
+    });
+
+
+
+
+
 
 
 
@@ -65,8 +157,95 @@ $(document).ready( function() {
     $(".pause").click(function() {
         $(".track1")[0].pause();
     }); 
+
+    // ////Bread Article/////////
+
+    $(".breadonelink").click(function() {
+        $(".appear").hide();
+        $(".breadone").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btnbreadone").click(function() {
+        $(".appear").hide();
+        $(".breadone").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".breadtwolink").click(function() {
+        $(".appear").hide();
+        $(".breadtwo").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btnbreadtwo").click(function() {
+        $(".appear").hide();
+        $(".breadtwo").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    // ///// Blood Article ///////
+
+    $(".bloodonelink").click(function() {
+        $(".appear").hide();
+        $(".bloodone").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btnbloodone").click(function() {
+        $(".appear").hide();
+        $(".bloodone").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".bloodtwolink").click(function() {
+        $(".appear").hide();
+        $(".bloodtwo").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btnbloodtwo").click(function() {
+        $(".appear").hide();
+        $(".bloodtwo").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".bloodthreelink").click(function() {
+        $(".appear").hide();
+        $(".bloodthree").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btnbloodthree").click(function() {
+        $(".appear").hide();
+        $(".bloodthree").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".bloodfourlink").click(function() {
+        $(".appear").hide();
+        $(".bloodfour").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btnbloodfour").click(function() {
+        $(".appear").hide();
+        $(".bloodfour").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    // //// Cook Article ////////
+
+    $(".cookonelink").click(function() {
+        $(".appear").hide();
+        $(".cookone").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btncookone").click(function() {
+        $(".appear").hide();
+        $(".cookone").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".cooktwolink").click(function() {
+        $(".appear").hide();
+        $(".cooktwo").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btncooktwo").click(function() {
+        $(".appear").hide();
+        $(".cooktwo").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
     
-    ///////////////
+    ///////Table of Contents////////
 
     $(".nav").click(function() {
         $(".appear").toggle();
@@ -111,6 +290,27 @@ $(document).ready( function() {
         $(".appear").hide();
         $(".fore").get(0).scrollIntoView({ behavior: "smooth" });
     }); 
+
+    $(".cookarticlelink").click(function() {
+        $(".appear").hide();
+        $(".cookarticle").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btncookarticle").click(function() {
+        $(".appear").hide();
+        $(".cookarticle").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".indexlink").click(function() {
+        $(".appear").hide();
+        $(".index").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
+    $(".btnindex").click(function() {
+        $(".appear").hide();
+        $(".index").get(0).scrollIntoView({ behavior: "smooth" });
+    }); 
+
 
 
 
